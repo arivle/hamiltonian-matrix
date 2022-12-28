8ec538d1841fbea943b046418fb3265bbef224e7
arivle <arivleone@gmail.com>
2022-12-28 19:40:09 +0700

    add fermi matrix function and normalization of psi

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <complex>
 #include <fstream>
+#include <iomanip>
+#include <iterator>
+#include<cmath>
 
 using namespace std;
 
@@ -121,14 +124,117 @@ complex<double> dot_product(vector<complex<double>> &a, vector<complex<double>> 
 
 }
 
-void write_csv(string filename, string colname, vector<complex<double>> vect, int nmat){
-    ofstream myFile(filename);
-
-    myFile << colname<<"\n";
+complex<double> normalize_psi(vector<complex<double>> &a, int num, int nmat){
+    complex<double> sum0n = 0.0;
+    complex<double> sum0(nmat);
 
     for (int i = 0; i < nmat; i++)
     {
-        myFile<<vect[i]<<"\n";
+        sum0n = a[i] * a[i];
+        if (num==0)
+        {
+            sum0[i] = sqrt(4*sum0n[i]);
+        }
+        else if (num==1)
+        {
+            sum0[i] = sqrt(2*sum0n[i]);
+        }
+   
+    }
+    return sum0;    
+}
+
+vector<complex<double>> fermi(int ff, int c, vector<complex<double>>& in, vector<complex<double>>& fermiDirac, vector<complex<double>>& fermiDirac_t1, complex<double> psi0N, int nx, int ny, float dt, int nm){
+    int T = 300;
+    double Kb = 0.695;
+    int pot = 0;
+    int mu = 0;
+    int nrx = nx/2;
+    int nry = ny/2;
+    int sx1 = 0;
+    int sy1 = 0;
+    int sx2 = 0;
+    int sy2 = 0;
+
+    double TEn = T*Kb;
+    complex<double> potN = pot / psi0N;
+    complex<double> bN = psi0N / TEn;
+    complex<double> aN = 1/psi0N;
+
+    complex<double> ct (cos(-dt * 1),0);
+    complex<double> st (0, (-1 * sin(-dt*1)));
+
+    for (int i = 0; i < nx; i++)
+    {
+        for (int j = 0; j < ny; j++)
+        {
+            if (i==j)
+            {
+                fermiDirac[i][j] = ct * sqrt((bN*potN) - (mu/TEn));
+            }
+            
+        }
+        
+    }
+
+    // filling the inverse matrix of fermi dirac: 2x2 block diagonal parth with even index
+    for (int i = 0; i < nrx; i++)
+    {
+        for (int j = 0; j < nry; j++)
+        {
+            if (i == j)
+            {
+                sx1 = 2*i;
+                sy1 = 2*j;
+                fermiDirac[sx1][sy1+1] = -st * sqrt((bN*potN) - (mu/TEn));
+                fermiDirac[sx1+1][sy1] = -st * sqrt((bN*potN) - (mu/TEn));
+            }
+            
+        }
+        
+    }
+
+    // filling the invers matrix of fermi dirac: 2x2 block diagonal with odd index
+    for (int j = 0; j < ny; i++)
+    {
+        sx2 = 2*0+0;
+        sy2 = 2*j+0;
+        fermiDirac_t1[sx2][sy2] = ct*fermiDirac[sx2][sy2] - st*fermiDirac[nm][sy2];
+        fermiDirac_t1[nm][sy2] = -st*fermiDirac[sx2][sy2] + ct*fermiDirac[nm][sy2];
+        sx2 = 0;
+        sy2 = 0;
+
+    }
+
+    for (int i = 0; i < nrx-1; i++)
+    {
+        for (int j = 0; j < ny; j++)
+        {
+            sx2 = 2*i+1;
+            sy2 = j;
+            fermiDirac_t1[sx2][sy2] = ct*fermiDirac[sx2][sy2] - st*fermiDirac[sx2+1][sy2];
+            fermiDirac_t1[sx2+1][sy] = -ct*fermiDirac[sx2][sy2] + ct*fermiDirac[sx2+1][sy2];
+        }
+        
+    }
+    
+    
+    
+
+    
+
+}
+
+void write_txt(string filename, vector<complex<double>> vect, int nmat){
+    ofstream myFile(filename);
+    // myFile.open(filename, ios::out | ios::binary);
+
+    // myFile << colname<<"\n";
+
+    for (int i = 0; i < nmat; i++)
+    {
+        myFile<<setprecision(16)<<vect[i]<<"\n";
+        // myFile.write(vect[i], sizeof(float));
     }
     myFile.close();   
 
@@ -150,6 +256,7 @@ int main(int argc, char const *argv[])
     float h = 0.01;
     /*just make my computational time shorter. please change this nstep as you need*/
     int nstep = 10; 
+    int nm = nstep-1;
     float ta = (nstep/2)*dt;
     float tau = 0.0;
     int jj = 0;
@@ -158,6 +265,14 @@ int main(int argc, char const *argv[])
    
     /*vector of initial wave. the vector contains random integer of -1 to 1*/
     vector<complex<double> > psi0(nmat);
+
+    /*vector of normalization psi*/
+    vector<complex<double> > psi0N(nmat);
+
+    /*vector of zeros for fermi function*/
+    vector<complex<double> > fermiDirac(nmat);
+    vector<complex<double> > fermiDirac_t1(nmat);
+
     complex<double> I = complex<double>(-1.0, 1.0);
     for (int i =0; i<nmat; i++)
     {
@@ -171,6 +286,9 @@ int main(int argc, char const *argv[])
     {
         psi0_conj[i]=conj(psi0[i]);
     }
+
+    psi0N = normalize_psi(psi0, 0, nmat);
+    psi0N = normalize_psi(psi0, 1, nmat);
     
     //this loop just for showing the array contents
     // cout<<"psi0: ";
@@ -201,7 +319,8 @@ int main(int argc, char const *argv[])
         tau = dt * jj;
 
     }
-    write_csv("corrf.txt", "correlationF", corrF, nmat);
+    complex<double> matrix_fermi = fermi(1, 0, corrF, fermiDirac, fermiDirac_t1, psi0N, nx, ny, dt, nm);
+    write_txt("corrf.txt", corrF, nmat);
     
     return 0;
 }
